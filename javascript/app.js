caa51223c8b37938562fea85745d3647b049cda3
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-23 20:58:21 -0700

    incremental step: places todo fetching into the view layer :sigh:

--- javascript/app.js
+++ javascript/app.js
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from 'components/Root'
 import configureStore from 'javascript/configureStore'
-import { fetchTodos } from 'utility/api'
-
-fetchTodos('all').then(todos => {
-  console.log(todos)
-})
 
 const store = configureStore()
 
--- javascript/components/VisibleTodoList.jsx
+++ javascript/components/VisibleTodoList.jsx
@@ -1,19 +1,41 @@
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import TodoList from 'components/TodoList'
 import { toggleTodo } from 'javascript/actions'
 import { getVisibleTodos } from 'reducers'
+import { fetchTodos } from 'utility/api'
+
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(this.props.filter, todos)
+    })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(this.props.filter, todos)
+      })
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />
+  }
+}
 
 function mapStateToProps(state, { params }) {
+  const filter = params.filter || 'all'
+
   return {
-    todos: getVisibleTodos({
-      state: state,
-      filter: params.filter || 'all'
-    })
+    todos: getVisibleTodos({state, filter}),
+    filter
   }
 }
 
 export default withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList))
+)(VisibleTodoList))
