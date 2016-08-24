6672f22596857835c05580ae3e720a8228314299
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-23 20:13:43 -0700

    switches to storing todos in an object rather than an array
    finally!

--- javascript/reducers/todos.js
+++ javascript/reducers/todos.js
@@ -1,27 +1,47 @@
+import { combineReducers } from 'redux'
 import todo from 'reducers/todo'
 
-export default function todos(state = [], action) {
+export default combineReducers({
+  byId,
+  allIds
+})
+
+function getAllTodos(state) {
+  return state.allIds.map(id => state.byId[id])
+}
+
+export function byId(state = {}, action) {
   switch(action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action))
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action)
+      }
+    default:
+      return state
+  }
+}
+
+export function allIds(state = [], action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id]
     default:
       return state
   }
 }
 
 export function getVisibleTodos({state, filter}) {
+  const allTodos = getAllTodos(state)
+
   switch(filter) {
     case 'all':
-      return state
+      return allTodos
     case 'active':
-      return state.filter(todo => !todo.completed)
+      return allTodos.filter(todo => !todo.completed)
     case 'completed':
-      return state.filter(todo => todo.completed)
+      return allTodos.filter(todo => todo.completed)
     default:
       throw new Error(`Unknown filter: ${filter}`)
   }
