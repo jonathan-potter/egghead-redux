21172e1fb9c9ae8fb3c1a647fbdd4346b0b76095
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-24 17:03:14 -0700

    switches to using middleware packages

--- javascript/configureStore.js
+++ javascript/configureStore.js
@@ -1,48 +1,17 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import rootReducer from 'reducers'
 
-function wrapDispatchWithMiddlewares(store, middlewares) {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
-}
-
 export default function configureStore() {
-  const store = createStore(rootReducer)
   const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger)
+    middlewares.push(createLogger())
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
-  return store
-}
-
-const promise = store => next => action => {
-  if (typeof action.then === 'function') {
-    return action.then(next)
-  }
-
-  return next(action)
-}
-
-const logger = store => next => {
-  /* eslint-disable no-console */
-  if (!console.group) {
-    return next
-  }
-
-  return action => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-
-    return returnValue
-  }
-  /* eslint-enable no-console */
+  return createStore(
+    rootReducer,
+    applyMiddleware(...middlewares)
+  )
 }
